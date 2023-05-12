32405cc4665e4766d64800eeafd32caddf1d37c8
kailasnv <itskailasnv@gmail.com>
2023-05-12 11:23:29 +0530

    parallax scrolling

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lottie/lottie.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // scroll controller
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    // initiallizing the scroll controller
+    _scrollController = ScrollController()
+      ..addListener(() {
+        _onScoll();
+      });
+    super.initState();
+  }
+
+  double _scrollOffset = 0.0;
+
+  void _onScoll() {
+    setState(() {
+      _scrollOffset = _scrollController.offset;
+      print(_scrollOffset);
+    });
+  }
+
+  // layer speeds
+
+  double layer1Speed = 0.3; // layer-1 will be last , behind all layers
+  double layer2Speed = 0.4;
+  double layer3Speed = 0.5;
+  double layer4Speed = 0.5;
+
+  double sunSpeed = 0.18;
+  double containerSpeed = 0.5;
+  double textSpeed = -1;
+
+  // layer Horizontal values -/ used to increase height/size of the Layers, while scrolling
+  double layer1HSpeed = 0.1; // slow
+  double layer2HSpeed = 0.08;
+  double layer3HSpeed = 0.075;
+  double layer4HSpeed = 0.07; // fast - top layers will have more resizing speed
+
+  @override
+  Widget build(BuildContext context) {
+    // some screen-size values
+    var screenSize = MediaQuery.of(context).size;
+    var layoutHeight = screenSize.height * 2;
+
+    return Scaffold(
+      body: Container(
+        // container used for giving a gradient sky background
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+              Colors.yellow.shade200,
+              Colors.deepPurple.shade200,
+              Colors.blueGrey.withOpacity(0.7),
+            ])),
+        child: Stack(children: [
+          /*  image Layers  /  bottomLast  to TopFirst */
+          // sun
+          Positioned(
+              bottom: screenSize.height * 0.7 + _scrollOffset * sunSpeed,
+              left: _scrollOffset * layer1HSpeed * -1,
+              right: 150 + _scrollOffset * layer1HSpeed * -1,
+              child: Image.asset("assets/images/sun.png")),
+          // layer 1
+          Positioned(
+              bottom: screenSize.height * 0.5 + _scrollOffset * layer1Speed,
+              left: _scrollOffset * layer1HSpeed * -1,
+              right: _scrollOffset * layer1HSpeed * -1,
+              child: Image.asset(
+                "assets/images/mount2.png",
+              )),
+          Positioned(
+              bottom: screenSize.height * 0.5 + _scrollOffset * layer2Speed,
+              left: _scrollOffset * layer2HSpeed * -1,
+              right: _scrollOffset * layer2HSpeed * -1,
+              child: Image.asset("assets/images/backgnd1.png")),
+          // Text Layer
+          Positioned(
+              bottom: screenSize.height * 0.54 + _scrollOffset * textSpeed,
+              left: 0,
+              right: 0,
+              child: const Text(
+                  "MINATO", // I need to use GoogleFonts & TextStroke Paackages
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 55,
+                  ))),
+          Positioned(
+              bottom: screenSize.height * 0.5 + _scrollOffset * layer3Speed,
+              left: _scrollOffset * layer3HSpeed * -1,
+              right: _scrollOffset * layer3HSpeed * -1,
+              child: Image.asset("assets/images/backgnd2.png")),
+
+          Positioned(
+              bottom: screenSize.height * 0.5 + _scrollOffset * layer4Speed,
+              left: _scrollOffset * layer4HSpeed * -1,
+              right: _scrollOffset * layer4HSpeed * -1,
+              height: screenSize.height * 0.15,
+              child: Image.asset("assets/images/couples.png")),
+
+          /*
+              A Black Container 
+          */
+          Positioned(
+              bottom: -screenSize.height * 0.5 +
+                  (_scrollOffset * containerSpeed) +
+                  3,
+              left: 0,
+              right: 0,
+              height: screenSize.height,
+              child: Container(
+                color: Colors.deepPurple,
+                child: const Center(
+                  child: Text("Parallax Effect"),
+                ),
+              )),
+
+          /*/ some birds
+          Positioned(
+              bottom: 300,
+              left: 0,
+              right: 0,
+              height: screenSize.height * 0.15,
+              child: Lottie.asset("assets/json/birds.json")),  */
+
+          // this below Widget with( Positioned.fill) helps to make the screen scrollable ,
+          // and also the height : represents the whole layoutHeight of the page .
+          Positioned.fill(
+              child: SingleChildScrollView(
+            controller: _scrollController,
+            child: SizedBox(
+              height: layoutHeight,
+            ),
+          )),
+        ]),
+      ),
+    );
+  }
+}
